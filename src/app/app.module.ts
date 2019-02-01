cbcf2194c7e680a42a39e25fd4d67ec97d83a2e9
akshayr20 <akshay.rajput5394@gmail.com>
2019-02-01 14:56:36 +0530

    created 3 sections for auxilary outlet in same component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { Routes } from '@angular/router';
+import { Module1Module } from './module1/module1.module';
+import { Module2Module } from './module2/module2.module';
+import { Module3Module } from './module3/module3.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ContainerComponent],
+  imports: [BrowserModule, Module1Module, Module2Module, Module3Module, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
